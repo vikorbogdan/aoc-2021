b1c7bacb76fe2942c89280ae2570f43f2bbb121f
vikorbogdan <vikorbogdan@gmail.com>
2021-12-04 13:56:34 +0100

    day_3 solution broken part two needs fix

--- day_3/python/day_3.py
+++ day_3/python/day_3.py
@@ -9,13 +9,40 @@ def column(matrix, i):
     return [row[i] for row in matrix]
 
 
+def most_in_column(matrix, column_index):
+    c = Counter(column(matrix, column_index)).most_common()
+    if c[0][1] == c[-1][1]:
+        return 1
+    return c[0][0]
+
+
+def least_in_column(matrix, column_index):
+    c = Counter(column(matrix, column_index)).most_common()
+    if c[0][1] == c[-1][1]:
+        return 0
+    return c[-1][0]
+
+
 gamma_rate = "".join([
-    Counter(column(input_data, i)).most_common(1)[0][0] for i in range(len(input_data[0]))])
+    most_in_column(input_data, i) for i in range(len(input_data[0]))])
 epsilon_rate = "".join([
-    Counter(column(input_data, i)).most_common()[-1][0][0] for i in range(len(input_data[0]))])
+    least_in_column(input_data, i) for i in range(len(input_data[0]))])
 
 part_one_ans = int(gamma_rate, 2) * int(epsilon_rate, 2)
 print(part_one_ans)
 
-
-s
+oxygen_generator_rating_list = list(input_data)
+co2_scrubber_rating_list = list(input_data)
+for i in range(len(oxygen_generator_rating_list[0])):
+    oxygen_generator_rating_list = [j for j in oxygen_generator_rating_list if most_in_column(
+        oxygen_generator_rating_list, i) == j[i]]
+for i in range(len(co2_scrubber_rating_list[0])):
+    co2_scrubber_rating_list = [j for j in co2_scrubber_rating_list if least_in_column(
+        co2_scrubber_rating_list, i) == j[i]]
+ogr = int(oxygen_generator_rating_list[0], 2)
+co2 = int(co2_scrubber_rating_list[0], 2)
+del oxygen_generator_rating_list
+del co2_scrubber_rating_list
+part_two_ans = ogr*co2
+print(f"Solution of the first part: {part_one_ans}")
+print(f"Solution of the second part: {part_two_ans,ogr,co2}")
